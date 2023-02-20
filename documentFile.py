5d9d3e4c049d87e6502e49d6e34f66a8da5bc383
marifinstad <73528343+marifinstad@users.noreply.github.com>
2023-02-20 17:29:57 +0100

    ferdig oppgave 7 +

--- documentFile.py
+++ documentFile.py
@@ -1,7 +1,61 @@
 import aspose.words as aw
+import gameFile
+
 
 doc = aw.Document()
 builder = aw.DocumentBuilder(doc)
 
 builder.write("hei")
+
+#TASK 7
+counts = [0,0,0]
+gameFile.readChessWins(counts)
+
+table = builder.start_table()
+# insert cell 
+builder.insert_cell()
+table.auto_fit(aw.tables.AutoFitBehavior.AUTO_FIT_TO_CONTENTS)
+
+# set formatting and add text
+builder.cell_format.vertical_alignment = aw.tables.CellVerticalAlignment.CENTER
+builder.write("Wins ")
+
+# insert cell
+builder.insert_cell()
+builder.write("Lose")
+
+builder.insert_cell()
+builder.write("Remis")
+
+# end row
+builder.end_row()
+
+# insert another cell in the next row
+builder.insert_cell()
+
+# format row if required
+builder.row_format.height = 100
+builder.row_format.height_rule = aw.HeightRule.EXACTLY
+
+# format cell and add text
+builder.cell_format.orientation = aw.TextOrientation.HORIZONTAL
+builder.writeln(str(counts[0]))
+
+# insert another cell, set formatting and add text
+builder.insert_cell()
+builder.cell_format.orientation = aw.TextOrientation.HORIZONTAL
+builder.writeln(str(counts[1]))
+
+builder.insert_cell()
+builder.cell_format.orientation = aw.TextOrientation.HORIZONTAL
+builder.writeln(str(counts[2]))
+
+# end row
+builder.end_row()
+
+# end table
+builder.end_table()
+
+#END OF TASK 7
+
 doc.save("test.docx")
