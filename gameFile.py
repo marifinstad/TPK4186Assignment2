7538e823ecaf3d79f1bea43f48b756ab5eabd00a
marifinstad <73528343+marifinstad@users.noreply.github.com>
2023-02-20 17:17:21 +0100

    halve task 7

--- gameFile.py
+++ gameFile.py
@@ -8,7 +8,9 @@ import chess.pgn
 def importGameFromFile():
     try:
         with open("game.pgn", "r") as fp:
-            game = chess.pgn.read_game(fp)                   
+            game = chess.pgn.read_game(fp) 
+            black_username = game.headers['Black']
+            print(black_username)    
             fp.read()
             fp.close()
             return game
@@ -16,3 +18,25 @@ def importGameFromFile():
         print("Could not read file") 
     
 importGameFromFile()
+
+#TASK 7
+def readChessWins(counter):
+    try:
+        with open("Stockfish_15_64-bit.commented.[2600].pgn" ,"r") as fp:
+            for line in fp: 
+                line = line.rstrip()
+                if re.search("Result", line):
+                    if re.search("1-0", line):
+                        counter[0] = counter[0] + 1
+                    elif re.search("0-1", line):
+                        counter[1] = counter[1] + 1
+                    else:
+                        counter[2] = counter[2] + 1
+            fp.close()
+    except:
+        print("Could not read file") 
+        
+#TEST 7
+couts = [0,0,0]
+readChessWins(couts)
+print(couts)
